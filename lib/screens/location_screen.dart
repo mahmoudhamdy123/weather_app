84e3a07534f8e875b26939e4aaab4697aff91c33
Mahmoud Hamdy <mh2276419@gmail.com>
2019-09-13 23:42:01 +0200

    first commit

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,70 +1,200 @@
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
+import 'package:flutter/widgets.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  WeatherModel weather = WeatherModel();
+  int temperature;
+  String weatherIcon;
+  String cityName;
+  String weatherMessage;
+  String typedName;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        weatherIcon = 'Error';
+        weatherMessage = 'Unable to get weather data';
+        cityName = ' ';
+        return;
+      }
+      double temp = weatherData['main']['temp'];
+      double celsius = temp - 273.15;
+      print(celsius);
+      print(temp);
+      temperature = celsius.toInt();
+      var condition = weatherData['weather'][0]['id'];
+      weatherIcon = weather.getWeatherIcon(condition);
+      weatherMessage = weather.getMessage(temperature);
+      cityName = weatherData['name'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        leading: FlatButton(
+          onPressed: () async {
+            var weatherData = await weather.getLocationWeather();
+            updateUI(weatherData);
+          },
+          child: Icon(
+            Icons.add_location,
+            color: Colors.blue,
+            size: 40,
           ),
         ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () async {
+              Alert(
+                style: alertStyle,
+                  context: context,
+                  title: "City Name",
+                  content: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.location_city),
+                          labelText: 'City Name',
+                        ),
+                        onChanged: (value){
+                          typedName = value;
+                        },
+                      ),
+                    ],
+                  ),
+                  buttons: [
+                    DialogButton(
+                      onPressed: () async {
+                        if (typedName != null)  {
+                          var weatherData = await weather.getCityWeather(typedName);
+                          updateUI(weatherData);
+                        }
+                        Navigator.pop(context);
+                        },
+                      child: Text(
+                        "Get Weather",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )
+                  ]).show();
+            },
+            child: Icon(
+              Icons.location_city,
+              size: 40.0,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FlatButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.near_me,
-                      size: 50.0,
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.location_city,
-                      size: 50.0,
-                    ),
-                  ),
-                ],
-              ),
               Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      '32°',
-                      style: kTempTextStyle,
-                    ),
-                    Text(
-                      '☀️',
-                      style: kConditionTextStyle,
-                    ),
-                  ],
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 20,
+                ),
+              ),
+              Card(
+                elevation: 10,
+                child: Container(
+                  width:
+                      MediaQuery.of(context).orientation == Orientation.portrait
+                          ? MediaQuery.of(context).size.width * 0.9
+                          : MediaQuery.of(context).size.width * 0.7,
+                  height:
+                      MediaQuery.of(context).orientation == Orientation.portrait
+                          ? MediaQuery.of(context).size.height * 0.2
+                          : MediaQuery.of(context).size.height * 0.35,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: <BoxShadow>[],
+                  ),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Temperature',
+                        style: kTempTextStyle,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 100.0,
+                        lineWidth: 13.0,
+                        animation: true,
+                        percent: temperature / 100,
+                        center: new Text(
+                          "$temperature°",
+                          style: new TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20.0),
+                        ),
+                        circularStrokeCap: CircularStrokeCap.round,
+                        progressColor: Colors.blue,
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(right: 15.0),
-                child: Text(
-                  "It's 🍦 time in San Francisco!",
-                  textAlign: TextAlign.right,
-                  style: kMessageTextStyle,
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 20,
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  width:
+                      MediaQuery.of(context).orientation == Orientation.portrait
+                          ? MediaQuery.of(context).size.width * 0.9
+                          : MediaQuery.of(context).size.width * 0.7,
+                  height:
+                      MediaQuery.of(context).orientation == Orientation.portrait
+                          ? MediaQuery.of(context).size.height * 0.3
+                          : MediaQuery.of(context).size.height * 0.4,
+                  child: Text(
+                    '$weatherMessage in $cityName',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        bottomLeft: Radius.circular(8.0),
+                        bottomRight: Radius.circular(8.0),
+                        topRight: Radius.circular(68.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          offset: Offset(1.1, 1.1),
+                          blurRadius: 10.0),
+                    ],
+                  ),
                 ),
               ),
             ],
--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 25,
+  color: Colors.blue,
 );
 
 const kMessageTextStyle = TextStyle(
@@ -18,3 +20,39 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      icon: Icon(
+        Icons.location_city,
+        color: Colors.white,
+      ),
+      hintText: 'Enter City Name',
+      hintStyle: TextStyle(
+        color: Colors.grey,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+);
+
+var alertStyle = AlertStyle(
+  animationType: AnimationType.fromTop,
+  isCloseButton: false,
+  isOverlayTapDismiss: false,
+  descStyle: TextStyle(fontWeight: FontWeight.bold),
+  animationDuration: Duration(milliseconds: 400),
+  alertBorder: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20.0),
+    side: BorderSide(
+      color: Colors.blue,
+    ),
+  ),
+  titleStyle: TextStyle(
+    color: Color.fromRGBO(91, 55, 185, 1.0),
+  ),
+);
+
+
+
